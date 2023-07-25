c5f0342fc61df3e0a8cf67b69ce0dd0228f70af6
Ryan Sakurai <ryansakurai03@gmail.com>
2023-07-25 09:10:14 -0300

    implement deque

--- deque/deque.c
+++ deque/deque.c
@@ -3,3 +3,105 @@
  * 
  *  Author: Ryan Sakurai
  */
+
+#include "deque.h"
+
+
+void d_init(Deque *deque) {
+    deque->sentinel = malloc(sizeof(DNode));
+    deque->sentinel->prev = deque->sentinel->next = deque->sentinel;
+    deque->size = 0;
+}
+
+
+void d_destroy(Deque *deque) {
+    while(d_pop_first(deque, NULL));
+    free(deque->sentinel);
+    deque->sentinel = NULL;
+}
+
+
+void d_push_first(Deque *deque, T data) {
+    DNode *new = malloc(sizeof(DNode));
+    new->data = data;
+    new->prev = deque->sentinel;
+    new->next = deque->sentinel->next;
+    new->prev->next = new->next->prev = new;
+
+    deque->size++;
+}
+
+
+void d_push_last(Deque *deque, T data) {
+    DNode *new = malloc(sizeof(DNode));
+    new->data = data;
+    new->prev = deque->sentinel->prev;
+    new->next = deque->sentinel;
+    new->prev->next = new->next->prev = new;
+
+    deque->size++;
+}
+
+
+bool d_pop_first(Deque *deque, T *output) {
+    if(d_is_empty(*deque))
+        return false;
+
+    DNode *old = deque->sentinel->next;
+    if(output)
+        *output = old->data;
+
+    old->next->prev = old->prev;
+    old->prev->next = old->next;
+    free(old);
+    deque->size--;
+    
+    return true;
+}
+
+
+bool d_pop_last(Deque *deque, T *output) {
+    if(d_is_empty(*deque))
+        return false;
+
+    DNode *old = deque->sentinel->prev;
+    if(output)
+        *output = old->data;
+
+    old->next->prev = old->prev;
+    old->prev->next = old->next;
+    free(old);
+    deque->size--;
+    
+    return true;
+}
+
+
+bool d_get_first(Deque deque, T *output) {
+    if(d_is_empty(deque))
+        return false;
+
+    if(output)
+        *output = deque.sentinel->next->data;
+    return true;
+}
+
+
+bool d_get_last(Deque deque, T *output) {
+    if(d_is_empty(deque))
+        return false;
+
+    if(output)
+        *output = deque.sentinel->prev->data;
+    return true;
+}
+
+
+unsigned d_get_size(Deque deque) {
+    return deque.size;
+}
+
+
+bool d_is_empty(Deque deque) {
+    return d_get_size(deque) == 0;
+}
--- deque/deque.h
+++ deque/deque.h
@@ -3,3 +3,136 @@
  * 
  *  Author: Ryan Sakurai
  */
+
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int T;
+
+typedef struct DNode {
+    T data;
+    struct DNode *prev;
+    struct DNode *next;
+} DNode;
+
+typedef struct Deque {
+    DNode *sentinel;
+    unsigned size;
+} Deque;
+
+/**
+ * Initializes the deque
+ * 
+ * Parameters
+ * - Deque *deque
+ * 
+ * Returns: void
+ */
+void d_init(Deque *deque);
+
+/**
+ * Destroys the deque
+ * 
+ * Parameters
+ * - Deque *deque
+ * 
+ * Returns: void
+ */
+void d_destroy(Deque *deque);
+
+/**
+ * Pushes an item to the first position of the deque
+ * 
+ * Parameters
+ * - Deque *deque
+ * - T data: data to be pushed to the deque
+ * 
+ * Returns: void
+ */
+void d_push_first(Deque *deque, T data);
+
+/**
+ * Pushes an item to the last position of the deque
+ * 
+ * Parameters
+ * - Deque *deque
+ * - T data: data to be pushed to the deque
+ * 
+ * Returns: void
+ */
+void d_push_last(Deque *deque, T data);
+
+/**
+ * Pops the first item of the deque and stores it in a variable
+ * 
+ * Parameters
+ * - Deque *deque
+ * - T *output: variable where the popped item will be stored (can be NULL)
+ * 
+ * Returns: bool
+ * - If it was possible to pop the item
+ */
+bool d_pop_first(Deque *deque, T *output);
+
+/**
+ * Pops the last item of the deque and stores it in a variable
+ * 
+ * Parameters
+ * - Deque *deque
+ * - T *output: variable where the popped item will be stored (can be NULL)
+ * 
+ * Returns: bool
+ * - If it was possible to pop an item
+ */
+bool d_pop_last(Deque *deque, T *output);
+
+/**
+ * Stores the first item of the deque in a variable
+ * 
+ * Parameters
+ * - Deque deque
+ * - T *output: variable where the item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to get the item
+ */
+bool d_get_first(Deque deque, T *output);
+
+/**
+ * Stores the last item of the deque in a variable
+ * 
+ * Parameters
+ * - Deque deque
+ * - T *output: variable where the item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to get the item
+ */
+bool d_get_last(Deque deque, T *output);
+
+/**
+ * Returns the size of the deque
+ * 
+ * Parameters
+ * - Deque deque
+ * 
+ * Returns: unsigned
+ * - The size of the deque
+ */
+unsigned d_get_size(Deque deque);
+
+/**
+ * Returns if the deque is empty or not
+ * 
+ * Parameters
+ * - Deque deque
+ * 
+ * Returns: bool
+ * - If the deque is empty
+ */
+bool d_is_empty(Deque deque);
+
+#endif
