011f49bb09b1903d4bd793041f0cc9cccf5b8c4e
Ryan Sakurai <ryansakurai03@gmail.com>
2023-07-27 16:02:49 -0300

    implement linked list

--- list/linked-list/linked_list.c
+++ list/linked-list/linked_list.c
@@ -3,3 +3,183 @@
  * 
  *  Author: Ryan Sakurai
  */
+
+#include "linked_list.h"
+
+bool __end_reached(LLIterator iter) {
+    return iter.current_item->next == iter.list->sentinel;
+}
+
+bool __is_in_range(LLIterator iter) {
+    return iter.current_item != iter.list->sentinel;
+}
+
+void ll_init(LinkedList *list) {
+    list->sentinel = malloc(sizeof(LLNode));
+    list->sentinel->prev = list->sentinel->next = list->sentinel;
+    list->size = 0;
+}
+
+void ll_destroy(LinkedList *list) {
+    while(!ll_is_empty(*list))
+        ll_pop_first(list, NULL);
+    
+    free(list->sentinel);
+    list->sentinel = NULL;
+}
+
+void ll_push_first(LinkedList *list, T data) {
+    LLNode *new = malloc(sizeof(LLNode));
+    new->data = data;
+    new->next = list->sentinel->next;
+    new->prev = list->sentinel;
+    new->next->prev = new->prev->next = new;
+    list->size++;
+}
+
+void ll_push_last(LinkedList *list, T data) {
+    LLNode *new = malloc(sizeof(LLNode));
+    new->data = data;
+    new->next = list->sentinel;
+    new->prev = list->sentinel->prev;
+    new->next->prev = new->prev->next = new;
+    list->size++;
+}
+
+bool ll_pop_first(LinkedList *list, T *output) {
+    if(ll_is_empty(*list))
+        return false;
+
+    LLNode *old = list->sentinel->next;
+    if(output)
+        *output = old->data;
+
+    list->sentinel->next = old->next;
+    list->sentinel->next->prev = list->sentinel;
+    free(old);
+    list->size--;
+
+    return true;
+}
+
+bool ll_pop_last(LinkedList *list, T *output) {
+    if(ll_is_empty(*list))
+        return false;
+
+    LLNode *old = list->sentinel->prev;
+    if(output)
+        *output = old->data;
+
+    list->sentinel->prev = old->prev;
+    list->sentinel->prev->next = list->sentinel;
+    free(old);
+    list->size--;
+
+    return true;
+}
+
+bool ll_get_first(LinkedList list, T *output) {
+    if(ll_is_empty(list))
+        return false;
+
+    if(output)
+        *output = list.sentinel->next->data;
+    return true;
+}
+
+bool ll_get_last(LinkedList list, T *output) {
+    if(ll_is_empty(list))
+        return false;
+
+    if(output)
+        *output = list.sentinel->prev->data;
+    return true;
+}
+
+unsigned ll_get_size(LinkedList list) {
+    return list.size;
+}
+
+bool ll_is_empty(LinkedList list) {
+    return list.size <= 0;
+}
+
+bool ll_iter_init(LLIterator *iter, LinkedList *list) {
+    if(ll_is_empty(*list))
+        return false;
+
+    iter->list = list;
+    iter->current_item = list->sentinel->next;
+    return true;
+}
+
+bool ll_iter_next(LLIterator *iter) {
+    if(__end_reached(*iter))
+        return false;
+
+    iter->current_item = iter->current_item->next;
+    return true;
+}
+
+bool ll_get_current_item(LLIterator iter, T *output) {
+    if(!__is_in_range(iter))
+        return false;
+
+    if(output)
+        *output = iter.current_item->data;
+    return true;
+}
+
+bool ll_push_before_current_item(LLIterator *iter, T data) {
+    if(!__is_in_range(*iter))
+        return false;
+
+    LLNode *new = malloc(sizeof(LLNode));
+    new->data = data;
+    new->next = iter->current_item;
+    new->prev = iter->current_item->prev;
+    new->next->prev = new->prev->next = new;
+    return true;
+}
+
+bool ll_push_after_current_item(LLIterator *iter, T data) {
+    if(!__is_in_range(*iter))
+        return false;
+
+    LLNode *new = malloc(sizeof(LLNode));
+    new->data = data;
+    new->next = iter->current_item->next;
+    new->prev = iter->current_item;
+    new->next->prev = new->prev->next = new;
+    return true;
+}
+
+bool ll_pop_current_item(LLIterator *iter, T *output) {
+    if(!__is_in_range(*iter))
+        return false;
+
+    LLNode *old = iter->current_item;
+    if(output)
+        *output = old->data;
+
+    old->prev->next = old->next;
+    old->next->prev = old->prev;
+    iter->current_item = iter->current_item->next;
+    free(old);
+    iter->list->size--;
+
+    return true;
+}
+
+bool ll_search(LLIterator *iter, bool (*equals)(void *a, void *b), T key) {
+    LLIterator private_iter;
+    if(ll_iter_init(&private_iter, iter->list))
+        do {
+            if((*equals)(&key, &private_iter.current_item->data)) {
+                iter->current_item = private_iter.current_item;
+                return true;
+            }
+        } while(ll_iter_next(&private_iter));
+
+    return false;
+}
--- list/linked-list/linked_list.h
+++ list/linked-list/linked_list.h
@@ -3,3 +3,231 @@
  * 
  *  Author: Ryan Sakurai
  */
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int T;
+
+typedef struct LLNode {
+    T data;
+    struct LLNode *prev;
+    struct LLNode *next;
+} LLNode;
+
+typedef struct LinkedList {
+    LLNode *sentinel;
+    unsigned size;
+} LinkedList;
+
+typedef struct LLIterator {
+    LinkedList *list;
+    LLNode *current_item;
+} LLIterator;
+
+/**
+ * Initializes the list
+ * 
+ * Parameters
+ * - LinkedList *list
+ * 
+ * Returns: void
+ */
+void ll_init(LinkedList *list);
+
+/**
+ * Destroys the list
+ * 
+ * Parameters
+ * - LinkedList *list
+ * 
+ * Returns: void
+ */
+void ll_destroy(LinkedList *list);
+
+/**
+ * Pushes an item to the first place of the list
+ * 
+ * Parameters
+ * - LinkedList *list
+ * - T data: data to be pushed to the list
+ * 
+ * Returns: void
+ */
+void ll_push_first(LinkedList *list, T data);
+
+/**
+ * Pushes an item to the last place of the list
+ * 
+ * Parameters
+ * - LinkedList *list
+ * - T data: data to be pushed to the list
+ * 
+ * Returns: void
+ */
+void ll_push_last(LinkedList *list, T data);
+
+/**
+ * Pops the first item off the list and stores it in a variable
+ * 
+ * Parameters
+ * - LinkedList *list
+ * - T *output: variable where the popped item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to pop an item
+ */
+bool ll_pop_first(LinkedList *list, T *output);
+
+/**
+ * Pops the last item off the list and stores it in a variable
+ * 
+ * Parameters
+ * - LinkedList *list
+ * - T *output: variable where the popped item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to pop an item
+ */
+bool ll_pop_last(LinkedList *list, T *output);
+
+/**
+ * Stores the first item of the list in a variable
+ * 
+ * Parameters
+ * - LinkedList list
+ * - T *output: variable where the item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to get the item
+ */
+bool ll_get_first(LinkedList list, T *output);
+
+/**
+ * Stores the last item of the list in a variable
+ * 
+ * Parameters
+ * - LinkedList list
+ * - T *output: variable where the item will be stored
+ * 
+ * Returns: bool
+ * - If it was possible to get the item
+ */
+bool ll_get_last(LinkedList list, T *output);
+
+/**
+ * Returns the size of the list
+ * 
+ * Parameters
+ * - LinkedList list
+ * 
+ * Returns: unsigned
+ * - The size of the list
+ */
+unsigned ll_get_size(LinkedList list);
+
+/**
+ * Returns if the list is empty
+ * 
+ * Parameters
+ * - LinkedList list
+ * 
+ * Returns: bool
+ * - If the list is empty
+ */
+bool ll_is_empty(LinkedList list);
+
+/**
+ * Initializes the iterator in a list
+ * 
+ * Parameters
+ * - LLIterator *iter
+ * - LinkedList *list: list the iterator will point to
+ * 
+ * Returns: bool
+ * - True if it was possible to initialize it
+ * - False if the list is empty and it wasn't possible
+ */
+bool ll_iter_init(LLIterator *iter, LinkedList *list);
+
+/**
+ * Moves the iterator to the next item in the list
+ * 
+ * Parameters
+ * - LLIterator *iter
+ * 
+ * Returns: bool
+ * - True if it was possible
+ * - False if the end of the list was reached and it wasn't possible
+ */
+bool ll_iter_next(LLIterator *iter);
+
+/**
+ * Stores the item currently being pointed by the iterator in a variable
+ * 
+ * Parameters
+ * - LLIterator iter
+ * - T *output - variable where the item is going to be stored
+ * 
+ * Returns: bool
+ * - True, if it was possible to get the item
+ * - False, if the iterator isn't pointing to an item and it wasn't possible
+ */
+bool ll_get_current_item(LLIterator iter, T *output);
+
+/**
+ * Pushes an item before the item currently being pointed by the iterator
+ * 
+ * Parameters
+ * - LLIterator *iter
+ * - T data: data to be pushed to the list
+ * 
+ * Returns: bool
+ * - True, if it was possible to push the item
+ * - False, if the iterator isn't pointing to an item and it wasn't possible
+ */
+bool ll_push_before_current_item(LLIterator *iter, T data);
+
+/**
+ * Pushes an item after the item currently being pointed by the iterator
+ * 
+ * Parameters
+ * - LLIterator *iter
+ * - T data: data to be pushed to the list
+ * 
+ * Returns: bool
+ * - True, if it was possible to push the item
+ * - False, if the iterator isn't pointing to an item and it wasn't possible
+ */
+bool ll_push_after_current_item(LLIterator *iter, T data);
+
+/**
+ * Pops and stores the item currently being pointed by the iterator in a variable
+ * 
+ * Parameters
+ * - LLIterator *iter
+ * - T *output: variable where the item is going to be stored
+ * 
+ * Returns: bool
+ * - True, if it was possible to pop the item
+ * - False, if the iterator isn't pointing to an item and it wasn't possible
+ */
+bool ll_pop_current_item(LLIterator *iter, T *output);
+
+/**
+ * Moves the iterator to a certain item, if it's possible to find it
+ * 
+ * Parameters
+ * - LinkedList *iter
+ * - bool (*equals)(void *a, void *b): function used to find the item
+ * - T key: key to be searched for
+ * 
+ * Returns: bool
+ * - If it was possible to find it
+ */
+bool ll_search(LLIterator *iter, bool (*equals)(void *a, void *b), T key);
+
+#endif
